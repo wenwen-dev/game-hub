4d867f51951adcd9fe5eab5d78684f94c05147fc
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-06-05 10:57:22 -0700

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from './components/SortSelector';
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -34,7 +35,7 @@ function App() {
         <GridItem area={'aside'} paddingX={5}>
           <GenreList
             selectedGenre={gameQuery.genre}
-            onSelectGenre={genre => setGameQuery({ ...gameQuery, genre })} //?
+            onSelectGenre={genre => setGameQuery({ ...gameQuery, genre })}
           />
         </GridItem>
       </Show>
@@ -46,7 +47,12 @@ function App() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortSelector />
+          <SortSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={sortOrder =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
